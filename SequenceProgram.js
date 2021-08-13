e832f1ef4d40b322e40aaa9ee0d12dd51a99d40e
Jebakani <vjishwarya@gmail.com>
2021-08-13 11:41:25 +0530

    [JEBAKANI] Add:Displaying the single digit using the random function

--- SequenceProgram.js
+++ SequenceProgram.js
@@ -1,1 +1,4 @@
 console.log("Executing Sequence problems");
+//P1-Displaying the Single digit using random function
+let singleDigit=Math.floor(Math.random()*10);
+console.log("P1-Single digit:"+singleDigit);
