f499a39ef67b5dde94c16bb0fc98776afe273ef2
Jebakani <vjishwarya@gmail.com>
2021-08-13 17:38:16 +0530

    [JEBAKANI] Add:Finding the factorial of the number

---
+++ ForLoopConstruct/factorial.js
@@ -0,0 +1,11 @@
+ //read input from the user
+ const prompt = require('prompt-sync')();
+ var num=prompt('Enter the number:');
+ num=parseInt(num);
+ let fact=1;
+ //finding the factorial of the number
+ for(let i=1;i<=num;i++)
+ {
+     fact*=i;
+ }
+ console.log("The factorial of "+num+ " is :"+fact);
