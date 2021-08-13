75ebd7878ca42e2cc7c61f069c1c6283296dbf1f
Jebakani <vjishwarya@gmail.com>
2021-08-13 16:21:54 +0530

    [JEBAKANI] Add: Calculating the powers of two and printing the table

---
+++ ForLoopConstruct/PowersOfTwoTable.js
@@ -0,0 +1,13 @@
+var prompt=require('prompt-sync')();
+var num=prompt('Enter the value of n: ');
+//if n value greater than 256 then stop the value
+if (num > 256) {
+    console.log("Number should be less than 256");
+}
+//else print the tables 
+else
+{
+for (let i = 0; i <= num; i++) {
+    console.log("2 ^ " + i + " = " + Math.pow(2, i));
+}
+}
