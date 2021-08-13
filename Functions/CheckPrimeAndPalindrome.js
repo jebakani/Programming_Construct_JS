1b99811b2bef6f8aa745a59ae74d33fb77cfc071
Jebakani <vjishwarya@gmail.com>
2021-08-13 19:11:06 +0530

    [JEBAKANI] Add: Adding function to check palindrome or not

--- Functions/CheckPrimeAndPalindrome.js
+++ Functions/CheckPrimeAndPalindrome.js
@@ -10,6 +10,24 @@ function IsPrime(num)
     }
     return true;
 }
+function IsPalindrome(num)
+{
+  let res=0;
+  let temp=num;
+  while(temp!=0)
+  {
+      res=(res*10)+(temp%10);
+      temp=Math.floor(temp/10);
+  }
+  if(num==res)
+  {
+      return true;
+  }
+  else
+  {
+      return false;
+  }
+}
 //funtion to print the result
 function CheckPrimeResult(res,num)
 {
@@ -25,5 +43,14 @@ function CheckPrimeResult(res,num)
 //get inpu from the user
 var prompt = require('prompt-sync')();
 var num = prompt('Enter the value of n: ');
-var res=IsPrime(num);
+let res=IsPrime(num);
 CheckPrimeResult(res,num);
+res=IsPalindrome(num);
+if(res)
+{
+    console.log("number is palindrome");
+}
+else
+{
+    console.log("number is not palindrome");
+}
