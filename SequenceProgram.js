7264fb55db827e2f475d675c339100701ae75d22
Jebakani <vjishwarya@gmail.com>
2021-08-13 11:42:59 +0530

    [JEBAKANI] Add: Displaying the Dice number between 1-6 using random function

--- SequenceProgram.js
+++ SequenceProgram.js
@@ -2,3 +2,7 @@ console.log("Executing Sequence problems");
 //P1-Displaying the Single digit using random function
 let singleDigit=Math.floor(Math.random()*10);
 console.log("P1-Single digit:"+singleDigit);
+
+//P2-Using random getting the Dice number between 1-6
+let dice=Math.floor(Math.random()*6)+1;
+console.log("P2-Display the Dice number:"+dice);
